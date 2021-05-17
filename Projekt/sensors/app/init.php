927d2bf17165bbeea945d627ee3c0a595688d65c
Daniel Dietrich <daniel.dietrich@tutamail.com>
2021-05-17 18:23:41 +0200

    Zmieniono nazwę folderu projektu

--- Projekt/sensors/app/init.php
+++ Projekt/sensors/app/init.php
@@ -5,7 +5,7 @@ require_once 'core/controller.php';
 require_once 'core/database.php';
 
 // Update root path
-define('ROOT_PATH', 'http://localhost/czujniki/public/');
+define('ROOT_PATH', 'http://localhost/sensors/public/');
 define('CSS_PATH', ROOT_PATH . 'css/');
 define('IMG_PATH', ROOT_PATH . 'img/');
 define('JS_PATH', ROOT_PATH . 'js/');

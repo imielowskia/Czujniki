676cc6b255671582779f7b56b0b47e9e631ae0c4
Kacper Furman <84005621+duda13133@users.noreply.github.com>
2021-05-28 11:33:41 +0200

    Add files via upload

---
+++ Projekt/sensors/public/js/skrypt.php
@@ -0,0 +1,32 @@
+<?php
+
+$sql = "SELECT * FROM waypoint";
+
+$dbhost = "localhost";
+$dbuser = "SENSOR";
+$dbpass = "haslosensora";
+$dbname = "sensors";
+$conn = mysqli_connect($dbhost, $dbuser, $dbpass, $dbname);
+
+$result = mysqli_query($conn, $sql);
+
+$geojson = array('type' => 'FeatureCollection', 'features' => array());
+
+while ($row = mysqli_fetch_assoc($result)) {
+    $marker = array(
+        'type' => 'Nazwa',
+        'features' => array(
+            'type' => 'Opis',
+            "geometry" => array(
+                'type' => 'Point',
+                'coordinates' => array(
+                    $row['szerokosc'],
+                    $row['dlugosc']
+                )
+            )
+        )
+    );
+    array_push($geojson['features'], $marker);
+}
+
+echo json_encode($geojson);

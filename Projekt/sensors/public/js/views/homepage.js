c364bbc94ac87f7cd4d2969bdab6021881f4f3f2
Kacper Furman <84005621+duda13133@users.noreply.github.com>
2021-05-28 11:16:19 +0200

    Update homepage.js

--- Projekt/sensors/public/js/views/homepage.js
+++ Projekt/sensors/public/js/views/homepage.js
@@ -1,8 +1,18 @@
-var mymap = L.map('mapid').setView([50.015, 22.68], 14);
-
-L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
-    maxZoom: 18,
-    id: 'mapbox/streets-v11',
-    tileSize: 512,
-    zoomOffset: -1
-}).addTo(mymap);
+var mymap = L.map('mapid').setView([ 50.01628836750675,22.677583694458008 ], 13);
+    L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+        attribution: 'Map data &copy; <a> href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a> href="https://www.mapbox.com/">Mapbox</a>',
+        maxZoom: 18,
+        id: 'mapbox/streets-v11',
+        tileSize: 512,
+        zoomOffset: -1,
+        accessToken: 'pk.eyJ1IjoiZHVkYTEzMTMzIiwiYSI6ImNrcDJwdmQ2dDFwMTUycW1jY2w4aHM0d2cifQ.jkBf9aZmMU6QUS9wHtoGBQ'
+    }).addTo(mymap);
+    $.ajax({
+        type:"Point",
+        url: '',
+        dataType: 'json',
+        success: console.log("Dane załadowane pomyślnie"),
+        error: function (xhr) {
+            alert(xhr.statusText)
+        }
+    })

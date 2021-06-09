a51a395fc17fa5adcb516f60bf120a51adc8fcdd
Kacper Furman <84005621+duda13133@users.noreply.github.com>
2021-06-09 10:33:49 +0200

    Update index.php

--- Projekt/sensors/app/views/homepage/index.php
+++ Projekt/sensors/app/views/homepage/index.php
@@ -62,6 +62,7 @@
     <!-- Skrypty odpowiedzialne za mapę -->
     <script src="<?= JS_PATH ?>leaflet.js"></script>
     <script src="<?= JS_PATH ?>views/homepage.js"></script>
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
 </body>
 
 </html>

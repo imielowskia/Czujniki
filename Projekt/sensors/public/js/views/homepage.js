5283e94397454d65fcfc1af4c14b922941960c02
Kacper Furman <84005621+duda13133@users.noreply.github.com>
2021-06-09 10:31:20 +0200

    Update homepage.js

--- Projekt/sensors/public/js/views/homepage.js
+++ Projekt/sensors/public/js/views/homepage.js
@@ -1,3 +1,4 @@
+<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
 var mymap = L.map('mapid').setView([ 50.01628836750675,22.677583694458008 ], 13);
     L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
         attribution: 'Map data &copy; <a> href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a> href="https://www.mapbox.com/">Mapbox</a>',
